dddbf521ab78a068e378e7a0eaee590c1e77248f
Guilherme Torres <guiatorres8@gmail.com>
2022-12-26 01:11:12 -0300

    day5

---
+++ day-05/highest_score.py
@@ -0,0 +1,11 @@
+student_scores = input("Input a list of student scores ").split()
+highest_score = 0
+
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest core in the class is: {highest_score}")
